b6a322a985e0ac5fc9ee08ef10c13386432e7d6a
Kfir Amoyal <kfiramoyal@gmail.com>
2026-01-29 15:47:30 +0200

    reorganize project structure - move all files to PhaseB

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -7,7 +7,7 @@ const v2_1 = require("firebase-functions/v2");
 const v2_2 = require("firebase-functions/v2");
 // Initialize Firebase Admin.
 admin.initializeApp();
-// Set global options
+// Set global options.
 (0, v2_1.setGlobalOptions)({
     timeoutSeconds: 120,
     memory: '512MiB',
